e4c87dfd978da2e212b41b2b91ef77dc65cd987f
gunhee <rjsgml771@naver.com>
2025-01-13 21:13:43 +0900

    feat: 추가 내용 작성

--- src/components/Main/AboutSection.tsx
+++ src/components/Main/AboutSection.tsx
@@ -28,14 +28,15 @@ export const AboutSection = () => {
             {visible === 2 && (
               <div>
                 <iframe
-                  src="https://giphy.com/embed/eSKQQ8bAe4LVehldJ5"
-                  width="387"
-                  height="480"
+                  src="https://giphy.com/embed/He4wudo59enf2"
+                  width="480"
+                  height="360"
+                  frameBorder="0"
                   className="giphy-embed"
                   allowFullScreen
                 ></iframe>
                 <p>
-                  <a href="https://giphy.com/gifs/movingin-guy-lars-moving-in-eSKQQ8bAe4LVehldJ5">
+                  <a href="https://giphy.com/gifs/satisfying-He4wudo59enf2">
                     via GIPHY
                   </a>
                 </p>
@@ -61,8 +62,8 @@ export const AboutSection = () => {
             >
               상상한 무궁무진한 아이디어를
             </strong>{' '}
-            제가 배운 기술로 구현해가는 과정이 재밌습니다. 항상 새로운 시도를
-            두려워하지 않는 프론트엔드 개발자가 되겠습니다.
+            제가 배운 기술로 구현해가는 과정이 재밌습니다. 항상 새로운 방향으로
+            도전해보는 프론트엔드 개발자가 되겠습니다.
           </p>
           <br />
           <br />
@@ -74,15 +75,14 @@ export const AboutSection = () => {
               className={`relative inline-block font-bold border-b border-gray-500 before:absolute before:left-0 before:bottom-0 before:h-[1px] before:w-0 before:bg-white before:origin-left before:transition-all before:duration-500 hover:before:w-full ${
                 visible === 1 && 'text-white'
               }`}
-              onMouseEnter={() => setVisible(1)}
+              onMouseEnter={() => setVisible(2)}
               onMouseLeave={() => setVisible(0)}
             >
-              “smooth waters run deep”
+              “욕망은 창의성, 사랑 그리고 장수를 촉진하는 강력한 강장제다.”
             </strong>{' '}
-            제가 가장 좋아하는 영어 격언 입니다. 거칠고 빠르게 흐르는 물보다
-            잔잔하지만 깊이 있게 흐르는 물처럼, 꾸준히 공부하며 나아갈 자신이
-            있습니다. 성실하게 쌓은 지식을 바탕으로, 회사에 실질적인 가치를 더할
-            수 있는 프론트엔드 개발자로 일하고 싶습니다.
+            제가 가장 좋아하는 명언 입니다. 더 좋은 서비스를 만들기 위해
+            만족하지 않고, 끊임없이 성장하는 개발자가 되겠습니다. 만족하지 않고
+            성장해 나가는 프론트엔드 개발자가 되겠습니다.
           </p>
         </div>
       </div>
